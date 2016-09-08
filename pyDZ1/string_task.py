2cde2a174778efec515415ad50c077029111bacb
Константин Недиков < >
2016-09-08 23:41:20 +0300

    Rewrited and repaired functions
    list_task.py ->full rewrited all functions; optimization.
    string_task.py ->full rewrited not_bad and front_back funcs;
    return instead "c=" in verbing.
    wordcount.py -> read_words func copied from dropbox file;
    added make_dict func;
    optimizated print_top;
    renamed some vars.

--- pyDZ1/string_task.py
+++ pyDZ1/string_task.py
@@ -1,51 +1,22 @@
-# Given a string, if its length is at least 3,
-# add 'ing' to its end.
-# Unless it already ends in 'ing', in which case
-# add 'ly' instead.
-# If the string length is less than 3, leave it unchanged.
-# Return the resulting string.
-#
-# Example input: 'read'
-# Example output: 'reading'
-def verbing(s):
-    if len(s)>= 3:
-        if s[-3:]!="ing":
-            s+="ing"
-        else: s=s[:-3]+"ly"    
-    return s
-print(verbing("reading"))
-# Given a string, find the first appearance of the
-# substring 'not' and 'bad'. If the 'bad' follows
-# the 'not', replace the whole 'not'...'bad' substring
-# with 'good'.
-# Return the resulting string.
-#
-# Example input: 'This dinner is not that bad!'
-# Example output: 'This dinner is good!'
-def not_bad(s):
-    catched = 0
-    for i in range(0,len(s)-3):
-        if s[i:i+3]=="not":
-            catched = 1
-            start = i
-        if s[i:i+3]=="bad" and catched:
-            s=s[:start]+"good"+s[i+3:]
-            catched = 0
-    return s
-print(not_bad("This dinner is not that bad! Yeah, thats really not bad."))
 
-# Consider dividing a string into two halves.
-# If the length is even, the front and back halves are the same length.
-# If the length is odd, we'll say that the extra char goes in the front half.
-# e.g. 'abcde', the front half is 'abc', the back half 'de'.
-#
-# Given 2 strings, a and b, return a string of the form
-#  a-front + b-front + a-back + b-back
-#
-# Example input: 'abcd', 'xy'
-# Example output: 'abxcdy'
+def verbing(s):
+	if len(s)>= 3:
+		if s[-3:]!="ing":
+			return s+"ing"
+		else: return s[:-3]+"ly"
+	else: return s		
+   
+print(verbing("reading"))
+
+def not_bad(s):
+	if s.find("not")<s.find("bad"):
+		s = s.replace(s[s.find("not") : s.find("bad") + 3], "good")
+	return s
+
+print(not_bad("This dinner is not that bad!"))
+
 def front_back(a, b):
-    x,y=len(a),len(b)
-    c=a[:x//2+x%2]+b[:y//2+y%2]+a[x//2+x%2:]+b[y//2+y%2:]
-    return c
+	x,y=len(a)//2+len(a)%2, len(b)//2+len(b)%2
+	c=a[:x]+b[:y]+a[x:]+b[y:]
+	return c
 print(front_back("aaaaa", "bbbb"))
--- pyDZ1/wordcount.py
+++ pyDZ1/wordcount.py
@@ -1,57 +1,47 @@
 import sys
+ 
+def read_words(filename):
+	words = []
+	with open(filename, "r") as f:
+		for line in f:
+			words.extend(line.lower().split())
+	return words
 
-def fread(fname):
-    file = open(fname)
-    dct=dict()
-    allwords = []
-    for line in file:
-        line.strip()
-        line = line.lower()
-        allwords.extend(line.split())
-    for wrd in allwords:
-        if wrd in dct:
-            dct[wrd] +=1
-        else: dct[wrd] = 1
-    return dct
+def make_dict(words):
+	dct=dict()
+	for wrd in words:
+		if wrd in dct:
+			dct[wrd] +=1
+		else: dct[wrd] = 1
+	return dct
 
-#sprint(fread("blabla.txt"))
-
-
-
-def print_words(fname):
-    dct = fread(fname)
-    sortedKeys = sorted(dct.keys())
-    for key in sortedKeys:
-        print(key, dct[key])
+def print_words(filename):
+	dct = make_dict(read_words(filename))
+	sortedKeys = sorted(dct.keys())
+	for key in sortedKeys:
+		print(key, dct[key])
+		
 import operator
-def print_top(fname):
-    dct = fread(fname)
-    #for k,v in dct.items()
-    sortedDct = sorted(dct.items(), key=operator.itemgetter(1))
-    j=0
-    for i in reversed(sortedDct):
-        j+=1
-        print(i[0],i[1])
-        if j==20: break
-    
-
-#print_words("blabla.txt")
-
+def print_top(filename):
+	dct = make_dict(read_words(filename))
+	sortedDct = sorted(dct.items(), key=operator.itemgetter(1), reverse=True)
+	for value in (sortedDct[:20]):
+		print(value[0],value[1])
 
 def main():
-    if len(sys.argv) != 3:
-        print('usage: ./wordcount.py {--count | --topcount} file')
-        sys.exit(1)
-
-    option = sys.argv[1]
-    filename = sys.argv[2]
-    if option == '--count':
-        print_words(filename)
-    elif option == '--topcount':
-        print_top(filename)
-    else:
-        print('unknown option: ' + option)
-        sys.exit(1)
-
+	if len(sys.argv) != 3:
+		print('usage: ./wordcount.py {--count | --topcount} file')
+		sys.exit(1)
+		
+	option = sys.argv[1]
+	filename = sys.argv[2]
+	if option == '--count':
+		print_words(filename)
+	elif option == '--topcount':
+		print_top(filename)
+	else:
+		print('unknown option: ' + option)
+		sys.exit(1)
+ 
 if __name__ == '__main__':
-    main()
+	main()
