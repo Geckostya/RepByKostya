159ac5e355f4dd65656e8cbb7063596d13e6d2da
Константин Недиков < >
2016-09-09 23:00:23 +0300

    returned upgrade old version

--- pyDZ1/list_task.py
+++ pyDZ1/list_task.py
@@ -1,16 +1,22 @@
 def remove_adjacent(lst):
-	i = 0
+	i,lst2 = 0, []
 	while i < len(lst)-1:
-		if lst[i] == lst[i + 1]:
-			lst=lst[:i]+lst[i+1:]
-		else: i += 1
-	return lst
-print (remove_adjacent([1, 1, 2, 1, 2, 2, 2, 3, 2, 4, 4, 4]))
+		if lst[i] != lst[i + 1]:
+			lst2+=[lst[i]]
+		i+=1;
+	if lst[-1] != lst2[-1]:
+		lst2+=[lst[-1]]
+	return lst2
+print (remove_adjacent([1, 1, 2, 1, 2, 2, 2, 3, 2, 4, 4, 5]))
 
 def linear_merge(lst1, lst2):
-	lst3, i ,j = [], 0, len(lst1)>0 and (len(lst1)<len(lst2) and len(lst1) or len(lst2))
-	while i<j:
-		lst3 += lst1[i]<lst2[i] and [lst1[i]]+[lst2[i]] or [lst2[i]]+[lst1[i]]
-		i+=1
-	return lst3+lst1[j:]+lst2[j:]
-print(linear_merge([1, 2], []))
+	lst3, i, j = [], 0, 0
+	while i<len(lst1) and j<len(lst2):
+		if lst1[i]<lst2[j]:
+			lst3 += [lst1[i]]
+			i+=1
+		else:
+			lst3 += [lst2[j]]
+			j+=1
+	return lst3+lst1[i:]+lst2[j:]
+print(linear_merge([0,2,18], [0,1,5,8]))
