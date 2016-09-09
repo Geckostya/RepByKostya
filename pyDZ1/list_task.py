4616f252fef248b02f7b60f3fee6bda1a44207a1
Константин Недиков < >
2016-09-09 19:33:37 +0300

    rewrited linear_merge, edited remove_adjacent
    replaced .pop()
    full rewrited func

--- pyDZ1/list_task.py
+++ pyDZ1/list_task.py
@@ -2,25 +2,15 @@ def remove_adjacent(lst):
 	i = 0
 	while i < len(lst)-1:
 		if lst[i] == lst[i + 1]:
-			lst.pop(i)
+			lst=lst[:i]+lst[i+1:]
 		else: i += 1
 	return lst
 print (remove_adjacent([1, 1, 2, 1, 2, 2, 2, 3, 2, 4, 4, 4]))
 
 def linear_merge(lst1, lst2):
-	lst3, i ,j = [], 0 ,0
-	while 1:
-		if lst1[i] < lst2[j]:
-			lst3+=[lst1[i]]
-			i += 1
-		else:
-			lst3+=[lst2[i]]
-			j += 1
-		if i == len(lst1)-1:
-			lst3+=lst2[j:]
-			break
-		if i == len(lst2)-1:
-			lst3+=lst1[i:]
-			break
-	return lst3
-print(linear_merge([2, 4, 5, 6], [1, 3, 5, 6, 7, 9]))
+	lst3, i ,j = [], 0, len(lst1)>0 and (len(lst1)<len(lst2) and len(lst1) or len(lst2))
+	while i<j:
+		lst3 += lst1[i]<lst2[i] and [lst1[i]]+[lst2[i]] or [lst2[i]]+[lst1[i]]
+		i+=1
+	return lst3+lst1[j:]+lst2[j:]
+print(linear_merge([1, 2], []))
