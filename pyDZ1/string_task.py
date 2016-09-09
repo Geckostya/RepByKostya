8dbbb168ac15317b7a33fd74ee0ea32ff947ff96
Константин Недиков < >
2016-09-09 23:47:45 +0300

    not_bad optimization

--- pyDZ1/string_task.py
+++ pyDZ1/string_task.py
@@ -1,6 +1,6 @@
 def verbing(s):
 	if len(s)>=3:
-		if s[-3:] == "ing":
+		if s[-3:]=="ing":
 			return s+"ly"
 		return s+"ing"
 	return s
@@ -9,13 +9,13 @@ print(verbing("reading"))
 
 def not_bad(s):
 	n,b = s.find("not"), s.find("bad")
-	if n<b and (n>0 or s[:3]=="not"):
-		return s.replace(s[n : b + 3], "good")
+	if n<b and (n>0 or s[:3]=="not"):#[:3] - it's a first 3 letters; if "not something bad bla bla" n==0, but we have "not"
+		return s[:n]+"good"+s[b+3:]
 	return s
 	
 print(not_bad("This dinner is not that bad!"))
 
 def front_back(a, b):
-	x,y=(len(a)+1)//2, (len(b)+1)//2
+	x,y = (len(a)+1)//2, (len(b)+1)//2
 	return a[:x]+b[:y]+a[x:]+b[y:]
 print(front_back("aaaaa", "bbbb"))
