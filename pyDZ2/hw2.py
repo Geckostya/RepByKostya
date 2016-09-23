fa49bc7185cfaefd735d758d3bb315f1f714a4b7
Константин Недиков < >
2016-09-23 19:03:02 +0300

    export format changed

--- pyDZ2/hw2.py
+++ pyDZ2/hw2.py
@@ -2,27 +2,23 @@ import os, sys
 import hashlib
  
 def findDup(parent):
-    dups = {}
-    for dirName, subdirs, fileList in os.walk(parent):
-        for filename in fileList:
-            path = os.path.join(dirName, filename)
-            hash = hashfile(path)
-            if hash in dups:
-                dups[hash].append(path)
-            else:
-                dups[hash] = [path]
-    return dups
- 
+	dups = {}
+	for dirName, subdirs, fileList in os.walk(parent):
+		for filename in fileList:
+			if filename[0]!='.':
+				path = os.path.join(dirName, filename)
+				hash = hashfile(path)
+				if hash in dups:
+					dups[hash].append(path[leng:])
+				else:
+					dups[hash] = [path[leng:]]
+	return dups
  
 def hashfile(path):
-    file = open(path, 'rb')
-    hasher = hashlib.md5()
-    buf = file.read(4096)
-    while len(buf) > 0:
-        hasher.update(buf)
-        buf = file.read(4096)
-    file.close()
-    return hasher.hexdigest()
+    digest = hashlib.md5()
+    with open(path, "rb") as f:
+        digest.update(f.read())
+    return digest.hexdigest()
  
 if len(sys.argv) > 1:
     dups = {}
@@ -34,8 +30,6 @@ if len(sys.argv) > 1:
     #print
     results = list(filter( lambda x: len(x) > 1, dups.values()))
     for result in results:
-        for subresult in result:
-            print(subresult[leng:], end = ":")
-        print()
+        print(":".join(result))
 else:
-    print('Usage: python dupFinder.py folder or python dupFinder.py folder1 folder2 folder3')
+    print('wrong format')
