e1133f3d9cab3d2c81a98cf52a288904558eacf8
Константин Недиков < >
2016-09-24 16:48:43 +0300

    tabs fixed

--- pyDZ2/hw2.py
+++ pyDZ2/hw2.py
@@ -1,6 +1,6 @@
 import os, sys
 import hashlib
- 
+
 def findDup(parent):
 	dups = {}
 	for dirName, subdirs, fileList in os.walk(parent):
@@ -13,23 +13,23 @@ def findDup(parent):
 				else:
 					dups[hash] = [path[leng:]]
 	return dups
- 
+
 def hashfile(path):
-    digest = hashlib.md5()
-    with open(path, "rb") as f:
-        digest.update(f.read())
-    return digest.hexdigest()
- 
+	digest = hashlib.md5()
+	with open(path, "rb") as f:
+		digest.update(f.read())
+	return digest.hexdigest()
+
 if len(sys.argv) > 1:
-    dups = {}
-    i = sys.argv[1]
-    leng = len(i)+1
-    if os.path.exists(i):
-        # Find the dups
-        dups = findDup(i)
-    #print
-    results = list(filter( lambda x: len(x) > 1, dups.values()))
-    for result in results:
-        print(":".join(result))
+	dups = {}
+	i = sys.argv[1]
+	leng = len(i)+1
+	if os.path.exists(i):
+		# Find the dups
+		dups = findDup(i)
+	#print
+	results = list(filter( lambda x: len(x) > 1, dups.values()))
+	for result in results:
+		print(":".join(result))
 else:
-    print('wrong format')
+	print('wrong format')
