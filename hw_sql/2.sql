bf302285ed88a2f1a97ebf3eaee1bd02d6b1ee79
linuh <kostya55516@mail.ru>
2016-12-03 22:00:28 +0300

    corrected 2, 5

--- hw_sql/2.sql
+++ hw_sql/2.sql
@@ -1,7 +1,9 @@
-select Country.Name, Rate
-from Country inner join LiteracyRate on Code = CountryCode
+select Name, max(Rate)
+from 
+(
+	select Name, Rate
+	from Country inner join LiteracyRate on Code = CountryCode
 
-group by CountryCode
-having max(Year) = Year
-
-order by Rate desc limit 1;
+	group by CountryCode
+	having max(Year) = Year
+);
--- hw_sql/5.sql
+++ hw_sql/5.sql
@@ -1,3 +1,7 @@
 select GovernmentForm, max(SurfSum)
-from (select GovernmentForm, sum(Country.SurfaceArea) as SurfSum
-	  from Country);
+from 
+(
+	select GovernmentForm, sum(Country.SurfaceArea) as SurfSum
+	from Country
+	group by GovernmentForm
+);
